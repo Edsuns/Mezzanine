79362eb8a1d0cd515235a8524c29c5e66690bc36
Edsuns <edsuns@qq.com>
2021-05-16 17:24:15 +0800

    Break the length limit of String constants

--- mezzanine-compiler/src/main/java/com/anthonycr/mezzanine/function/GenerateFileStreamTypeSpecFunction.kt
+++ mezzanine-compiler/src/main/java/com/anthonycr/mezzanine/function/GenerateFileStreamTypeSpecFunction.kt
@@ -14,26 +14,76 @@ import javax.lang.model.element.TypeElement
  */
 object GenerateFileStreamTypeSpecFunction : (Pair<TypeElement, String>) -> TypeSpec {
 
+    /**
+     * Constants have a limit of length, max to 65534.
+     */
+    private const val MAX_LENGTH = 65534
+
     override fun invoke(fileStreamPair: Pair<TypeElement, String>): TypeSpec {
-        val fileContents = StringEscapeUtils.escapeJava(fileStreamPair.second)
+        val fileContent = fileStreamPair.second
 
         val singleMethod = fileStreamPair.first.enclosedElements[0] as ExecutableElement
-
-        val methodSpec = MethodSpec
-                .methodBuilder(singleMethod.simpleName.toString())
-                .addAnnotation(Override::class.java)
-                .addModifiers(Modifier.PUBLIC)
-                .returns(String::class.java)
-                .addCode("return \"$1L\";\n", fileContents)
-                .build()
+        val methodName = singleMethod.simpleName.toString()
 
         return TypeSpec
                 .classBuilder(fileStreamPair.first.simpleName.toString())
                 .addModifiers(Modifier.PUBLIC)
                 .addModifiers(Modifier.STATIC)
                 .addSuperinterface(ClassName.get(fileStreamPair.first))
-                .addMethod(methodSpec)
+                .addMethods(createMethodSpecs(methodName, fileContent))
                 .build()
+    }
+
+    private fun createMethodSpecs(name: String, src: String): List<MethodSpec> {
+        val methodList = ArrayList<MethodSpec>()
+        if (src.length > MAX_LENGTH) {
+            var count = 0
+            var start = 0
+            while (start < src.length) {
+                val end = if (start + MAX_LENGTH >= src.length) {
+                    src.length
+                } else {
+                    src.offsetByCodePoints((start + MAX_LENGTH + 1).coerceAtMost(src.length - 1), -1)
+                }
+                val content = StringEscapeUtils.escapeJava(src.substring(start, end))
+                val methodSpec = MethodSpec
+                        .methodBuilder("__$name$count")
+                        .addModifiers(Modifier.PRIVATE)
+                        .returns(String::class.java)
+                        .addCode("return \"$1L\";\n", content)
+                        .build()
+                methodList.add(methodSpec)
+                start = end
+                count++
+            }
+
+            val code = StringBuilder()
+            for (i in 0 until count) {
+                if (i > 0) {
+                    code.append(" + ")
+                }
+                code.append("__$name$i").append("()")
+            }
+            methodList.add(MethodSpec
+                    .methodBuilder(name)
+                    .addAnnotation(Override::class.java)
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(String::class.java)
+                    .addCode("return $1L;\n", code)
+                    .build()
+            )
+        } else {
+            methodList.add(MethodSpec
+                    .methodBuilder(name)
+                    .addAnnotation(Override::class.java)
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(String::class.java)
+                    .addCode("return \"$1L\";\n", StringEscapeUtils.escapeJava(src))
+                    .build()
+            )
+        }
+
+        return methodList
     }
 
 }
